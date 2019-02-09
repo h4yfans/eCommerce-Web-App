0778d79d8ce6afc6e5549ae369624e094cc24f9c
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-09 17:42:07 +0300

    add cart to auth user when anon user after login

--- src/carts/models.py
+++ src/carts/models.py
@@ -3,12 +3,23 @@ from django.conf import settings
 from products.models import Product
 
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.DO_NOTHING)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamps = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
