c555f19ffccac3dd2886957013a2ad1229249b4c
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-10 19:33:18 +0300

    associate billing profile to order

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,6 +1,8 @@
 import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+
+from billing.models import BillingProfile
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
 
@@ -13,6 +15,7 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True, on_delete=models.CASCADE)
     order_id = models.CharField(max_length=120, blank=True)
     # billing_profile = ?
     # shipping_address
@@ -21,6 +24,7 @@ class Order(models.Model):
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
