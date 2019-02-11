b271ad9cd73a8ddb82e1766f1f116f1392777f26
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-11 18:06:14 +0300

    checkout success

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,4 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)  # change user password
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staff_user(self, email, password=None):
+        user = self.create(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_super_user(self, email, password=None):
+        user = self.create(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # confirm = models.BooleanField(default=False)
+    # confirm_date = models.DateTimeField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'  # username
+    # email and password are required by default
+    REQUIRED_FIELDS = []  # ['full_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
 
 
 class GuestEmail(models.Model):
