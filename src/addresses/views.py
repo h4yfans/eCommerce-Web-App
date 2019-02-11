51c0ed115cc0f8ec2a4017c1588d82240d811a52
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-11 14:16:39 +0300

    reuse addresses for checkout

--- src/addresses/views.py
+++ src/addresses/views.py
@@ -3,13 +3,11 @@ from django.utils.http import is_safe_url
 
 from addresses.forms import AddressForm
 from billing.models import BillingProfile
+from .models import Address
 
 
 def checkout_address_create_view(request):
     form = AddressForm(request.POST or None)
-    context = {
-        "form": form
-    }
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
@@ -33,6 +31,24 @@ def checkout_address_create_view(request):
 
         if is_safe_url(redirect_path, request.get_host()):
             return redirect(redirect_path)
-        else:
-            return redirect('cart:checkout')
     return redirect('cart:checkout')
+
+
+def checkout_address_reuse_view(request):
+    if request.user.is_authenticated:
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        if request.method == 'POST':
+            print(request.POST)
+            shipping_address = request.POST.get('shipping_address', None)
+            address_type = request.POST.get('address_type', 'shipping')
+            billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+            if shipping_address is not None:
+                qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
+                if qs.exists():
+                    request.session[address_type + '_address_id'] = billing_profile.id
+                if is_safe_url(redirect_path, request.get_host()):
+                    return redirect(redirect_path)
+
+        return redirect('cart:checkout')
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from accounts.views import login_page, register_page, guest_register_view
-from addresses.views import checkout_address_create_view
+from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
@@ -15,6 +15,7 @@ urlpatterns = [
     path('contact/', contact_page, name='contact'),
     path('login/', login_page, name='login'),
     path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
+    path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
     path('register/guest/', guest_register_view, name='guest_register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('cart/', include('carts.urls', namespace='cart')),
