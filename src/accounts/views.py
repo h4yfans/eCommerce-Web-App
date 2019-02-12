c60f35cabc7d71663005d30d378208b102fd478b
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-12 03:20:24 +0300

    login and register converted to class based view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, FormView
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from .models import GuestEmail
@@ -24,18 +26,20 @@ def guest_register_view(request):
     return redirect('register')
 
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+
+        email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             try:
@@ -45,12 +49,10 @@ def login_page(request):
             if is_safe_url(redirect_path, request.get_host()):
                 return redirect(redirect_path)
         else:
-            return redirect('/')
-    return render(request, 'accounts/login.html', context)
+            return super(LoginView, self).form_valid(form)
 
 
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request, 'accounts/register.html', {'form': form})
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('login')
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LogoutView
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts.views import login_page, register_page, guest_register_view
+from accounts.views import guest_register_view, RegisterView, LoginView
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from .views import home_page, about_page, contact_page
 
@@ -13,13 +13,13 @@ urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
-    path('login/', login_page, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('checkout/address/create/', checkout_address_create_view, name='checkout_address_create'),
     path('checkout/address/reuse/', checkout_address_reuse_view, name='checkout_address_reuse'),
     path('register/guest/', guest_register_view, name='guest_register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('cart/', include('carts.urls', namespace='cart')),
-    path('register/', register_page, name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('products/', include('products.urls', namespace='products')),
     path('search/', include('search.urls', namespace='search')),
 ]
